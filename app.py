2194499eece3c50f6f072a52697f5af4d2dc2d05
kcha0109 <kcha0109@student.monash.edu>
2023-08-04 15:52:11 +1000

    Kartik- SQLAchemy test

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import urllib
 
 app = Flask(__name__)
 
+params = urllib.parse.quote_plus("""
+Driver={ODBC Driver 18 for SQL Server};
+Server=tcp:week3proj.database.windows.net,1433;
+Database=ta22;
+Uid=kcha0109;
+Pwd=Monash@2024;
+Encrypt=yes;
+TrustServerCertificate=no;
+Connection Timeout=30;
+""")
+
+# Setup SQL Alchemy with the connection string
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional: Suppress warning
+
+# Create an instance of SQL Alchemy
+db = SQLAlchemy(app)
 
 
 @app.route('/')
