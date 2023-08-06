52c0f503678135c0237883eb7b1ba7806174986e
kcha0109 <kcha0109@student.monash.edu>
2023-08-06 11:24:00 +1000

    Kartik: Adding code for suburb functionality. Following changes made. 1. Added classes for Elecctricity emssions and gas emssions. 2. Created a route for emissions data. 3. Modified home.html for drop-downs to get suburbs postcode. 4. Created emissions.html.

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import urllib
 from calculate import calculate_carbon_emissions
+from sqlalchemy import func
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://kcha0109:Monash@2024@week3proj.database.windows.net:1433/ta22'
+params = urllib.parse.quote_plus("DRIVER={ODBC Driver 18 for SQL Server};"
+                                 "SERVER=week3proj.database.windows.net;"
+                                 "DATABASE=ta22;"
+                                 "UID=kcha0109;"
+                                 "PWD=Monash@2024;")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -22,11 +29,76 @@ class SuburbData(db.Model):
         return f'<Suburb {self.suburb}>'
 
 
+class ElecEmissionsData(db.Model):
+    __tablename__ = 'ElecEmissionsData'
+
+    year = db.Column(db.Integer, primary_key=True)
+    postcode = db.Column(db.Integer, db.ForeignKey('SuburbData.postcode'), primary_key=True)
+    suburb = db.Column(db.String(255))
+    emission_source = db.Column(db.String(255))
+    total_electricity_kwh = db.Column(db.Float)
+    average_intensity_kwh_per_customer_per_annum = db.Column(db.Float)
+    average_intensity_kwh_per_customer_per_day = db.Column(db.Float)
+    scope_2_kg_co2e = db.Column(db.Float)
+    scope_3_kg_co2e = db.Column(db.Float)
+    total_emissions_kg_co2e = db.Column(db.Float)
+    average_emissions_per_customer_kg_co2e = db.Column(db.Float)
+    average_emissions_energy_per_customer_kg_co2e_per_day = db.Column(db.Float)
+    rest_of_municipality_total_electricity_kwh = db.Column(db.Float)
+    rest_of_municipality_emissions_kg_co2e = db.Column(db.Float)
+    total_cost = db.Column(db.Float)
+    average_intensity_household_per_day = db.Column(db.Float)
+    average_intensity_household_per_month = db.Column(db.Float)
+    average_intensity_household_per_annum = db.Column(db.Float)
+
+
+class GasEmissionsData(db.Model):
+    __tablename__ = 'GasEmissionsData'
+
+    year = db.Column(db.Integer, primary_key=True)
+    postcode = db.Column(db.Integer, db.ForeignKey('SuburbData.postcode'), primary_key=True)
+    suburb = db.Column(db.String(255))
+    emission_source = db.Column(db.String(255))
+    total_gas_gj = db.Column(db.Float)
+    average_intensity_gj_per_customer_per_annum = db.Column(db.Float)
+    average_intensity_mj_per_customer_per_day = db.Column(db.Float)
+    scope_1_kg_co2e = db.Column(db.Float)
+    scope_3_kg_co2e = db.Column(db.Float)
+    total_emissions_kg_co2e = db.Column(db.Float)
+    average_emissions_per_customer_kg_co2e = db.Column(db.Float)
+    average_emissions_energy_per_customer_kg_co2e_per_day = db.Column(db.Float)
+    rest_of_municipality_total_gas_gj = db.Column(db.Float)
+    rest_of_municipality_emissions_kg_co2e = db.Column(db.Float)
+    total_cost = db.Column(db.Float)
+    average_intensity_household_per_day = db.Column(db.Float)
+    average_intensity_household_per_month = db.Column(db.Float)
+    average_intensity_household_per_annum = db.Column(db.Float)
+
+
 
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    suburbs = SuburbData.query.all()  # Query the database for all suburbs
+    return render_template("home.html", suburbs=suburbs)  # Pass the suburbs to the template
+
+@app.route('/emissions/<int:postcode>')
+def emissions(postcode):
+    try:
+        suburb_data = SuburbData.query.filter_by(postcode=postcode).first()
+
+        avg_gas_emission_per_customer = db.session.query(
+            func.avg(GasEmissionsData.average_emissions_per_customer_kg_co2e)).filter(
+            GasEmissionsData.postcode == postcode).scalar()
+        avg_elec_emission_per_customer = db.session.query(
+            func.avg(ElecEmissionsData.average_emissions_per_customer_kg_co2e)).filter(
+            ElecEmissionsData.postcode == postcode).scalar()
+
+        return render_template('emissions.html', suburb=suburb_data,
+                               avg_gas_emission_per_customer=avg_gas_emission_per_customer,
+                               avg_elec_emission_per_customer=avg_elec_emission_per_customer)
+    except Exception as e:
+        return str(e)
 
 @app.route("/about")
 def about():
